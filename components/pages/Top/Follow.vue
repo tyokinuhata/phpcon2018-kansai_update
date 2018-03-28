7215c339c83f2b1cc87b4a6d7b6c8dc3c13c860c
Kazuki Tobita <kazukiti201@gmail.com>
2018-03-28 15:00:52 +0900

    update Follow

--- components/pages/Top/Follow.vue
+++ components/pages/Top/Follow.vue
@@ -1,9 +1,18 @@
 <template>
-  <div class="p-follow">
-    <div class="c-ontainer">
+  <div class="home-Follow">
+    <div class="st-Container">
       <h2 class="home-Follow_Title">Follow SNS</h2>
-      <p>PHPカンファレンス関西のSNSアカウントをフォローして<br>最新情報チェック！</p>
-      <div class="home-Follow_SNS"><a class="home-Follow_Twitter" href="https://twitter.com/phpcon_kansai" target="_blank"><i class="fa fa-twitter fa-lg" aria-hidden="true"></i><span>&nbsp;twitterアカウント</span></a><a class="home-Follow_Facebook" href="https://www.facebook.com/pcon.kansai/" target="_blank"><i class="fa fa-facebook fa-lg" aria-hidden="true"></i><span>&nbsp;Facebookグループ</span></a></div>
+      <p>PHPカンファレンス関西のSNSアカウントをフォローして<br/>最新情報チェック！</p>
+      <div class="home-Follow_SNS">
+        <a href="https://twitter.com/phpcon_kansai" target="_blank" class="home-Follow_Twitter">
+          <i aria-hidden="true" class="fa fa-twitter fa-lg"></i>
+          <span>&nbsp;twitterアカウント</span>
+        </a>
+        <a href="https://www.facebook.com/pcon.kansai/" target="_blank" class="home-Follow_Facebook">
+          <i aria-hidden="true" class="fa fa-facebook fa-lg"></i>
+          <span>&nbsp;Facebookグループ</span>
+        </a>
+      </div>
     </div>
   </div>
 </template>
@@ -14,7 +23,8 @@
 <style lang="scss" scoped>
   @import "~assets/scss/library/_variable.scss";
   @import "~assets/scss/library/_mixin.scss";
-  .p-follow {
+
+  .home-Follow {
     width: auto;
     color: $clr_base;
     background-color: #fff;
@@ -22,14 +32,14 @@
 
     &_Title {
       @include secTitle($clr_base);
-     }
+    }
 
     &_SNS {
-       margin-top: 40px;
+      margin-top: 40px;
       .home-Follow_Twitter {
         @include button();
       }
-    .home-Follow_Facebook {
+      .home-Follow_Facebook {
         @include button();
       }
     }
