967fa6835c9b8369e9869391ded91756e174d20a
Kazuki Tobita <kazukiti201@gmail.com>
2018-03-28 16:07:08 +0900

    update TimeTable

--- components/pages/Top/TimeTable.vue
+++ components/pages/Top/TimeTable.vue
@@ -1,9 +1,20 @@
 <template>
-  <h1>タイムテーブル</h1>
+  <div class="home-TimeTable">
+    <div class="st-Container">
+      <h2 class="home-TimeTable_Title">TIME TABLE</h2>
+      <p class="home-TimeTable_Description">初心者から上級者まで楽しめるセッションが目白押し！これから始めようと思っていたフレームワークや、気になっていた技術についての情報が得られるかも！？</p>
+      <div class="home-TimeTable_Link">
+        <a href="https://twitter.com/phpcon_kansai" target="_blank" class="home-TimeTable_Anchor">
+          <i class="far fa-calendar-check"></i>
+          <span>&nbsp;タイムテーブルを確認</span>
+        </a>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 </script>
 
-<style>
+<style lang="scss" scoped>
 </style>
