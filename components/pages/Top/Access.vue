f306b26feb504f8d2fb3563e5f3ef9ddea1f2821
Kazuki Tobita <kazukiti201@gmail.com>
2018-03-30 14:31:47 +0900

    update Access

--- components/pages/Top/Access.vue
+++ components/pages/Top/Access.vue
@@ -2,6 +2,15 @@
   <div class="home-Access">
     <div class="st-Container">
       <h2 class="home-Access_Title">ACCESS</h2>
+      <div class="home-Access_Map"></div>
+      <div class="gome-Access_Description">
+        <p>グランフロント大阪&nbsp;北館&nbsp;タワーC&nbsp;8階</p>
+        <p>〒530-0011&nbsp;大阪市北区大深町3-1<br>&nbsp;グランフロント大阪&nbsp;タワーC8階</p>
+        <p>
+          <i class="far fa-calendar-check"></i>
+          <span>タワーCへはグランフロント北館1階からしかアクセスできません</span>
+        </p>
+      </div>
       <div class="home-Access_Link">
         <a href="https://twitter.com/phpcon_kansai" target="_blank" class="home-Access_Google">
           <i class="far fa-calendar-check"></i>
