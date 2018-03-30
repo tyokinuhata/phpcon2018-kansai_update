32fb7af34520d9f6af499cd98a56d8137f31185e
Kazuki Tobita <kazukiti201@gmail.com>
2018-03-30 14:42:00 +0900

    update Sponsor

--- components/pages/Top/Sponsor.vue
+++ components/pages/Top/Sponsor.vue
@@ -1,5 +1,45 @@
 <template>
-  <h1>スポンサー</h1>
+  <div class="home-Sponsor">
+    <div class="st-Container">
+      <h2 class="home-Sponsor_Title">SPONSOR</h2>
+      <p class="home-Sponsor_Description">初心者から上級者まで楽しめるセッションが目白押し！これから始めようと思っていたフレームワークや、気になっていた技術についての情報が得られるかも！？</p>
+      <div class="home-Sponsor_Sponsor">
+        <div class="home-Sponsor_Platinum">
+          <h3>プラチナスポンサー</h3>
+          <div></div>
+          <div></div>
+        </div>
+        <div class="home-Sponsor_Gold">
+          <h3>ゴールドスポンサー</h3>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div class="home-Sponsor_Silver">
+          <h3>シルバースポンサー</h3>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div class="home-Sponsor_Media">
+          <h3>メディアスポンサー</h3>
+          <div></div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
